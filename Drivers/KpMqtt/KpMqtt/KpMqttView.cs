72ad6afe6d035a93ae97241df1fd4205508b133a
2mik <m_shir@mail.ru>
2019-08-19 17:58:32 +0300

    KpMqtt: apply default VS formatting, replace tabs by spaces

--- Drivers/KpMqtt/KpMqtt/KpMqttView.cs
+++ Drivers/KpMqtt/KpMqtt/KpMqttView.cs
@@ -1,28 +1,24 @@
-﻿using System;
-
-namespace Scada.Comm.Devices
+﻿namespace Scada.Comm.Devices
 {
-	public class KpMqttView : KPView
-	{
-		public KpMqttView() : this(0)
-		{
-		}
+    public class KpMqttView : KPView
+    {
+        public KpMqttView() : this(0)
+        {
+        }
 
-		public KpMqttView(int number)
-			: base(number)
-		{
-		}
+        public KpMqttView(int number)
+            : base(number)
+        {
+        }
 
-		public override string KPDescr
-		{
-			get
-			{
-				return Localization.UseRussian ? 
-					"Библиотека КП для протокола MQTT." : 
-					"Device library for protocol MQTT.";
-			}
-		}
-
-	}
+        public override string KPDescr
+        {
+            get
+            {
+                return Localization.UseRussian ?
+                    "Библиотека КП для протокола MQTT." :
+                    "Device library for protocol MQTT.";
+            }
+        }
+    }
 }
-
--- Drivers/KpMqtt/KpMqtt/MqttPubTopic.cs
+++ Drivers/KpMqtt/KpMqtt/MqttPubTopic.cs
@@ -1,86 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using StriderMqtt;
-using Scada.Client;
+﻿using Scada.Client;
 using Scada.Data.Tables;
+using StriderMqtt;
+using System.Collections.Generic;
 
 namespace Scada.Comm.Devices
 {
-	public abstract class MQTTPubParam
-	{
-		public string TopicName {get;set;}
-		public MqttQos QosLevels {get;set;}
-		public bool Retain {get;set;}
-	}
+    public abstract class MQTTPubParam
+    {
+        public string TopicName { get; set; }
+        public MqttQos QosLevels { get; set; }
+        public bool Retain { get; set; }
+    }
 
-	public class MQTTPubTopic : MQTTPubParam
-	{
-		public int NumCnl { get; set; }
-		public string PubBehavior { get; set;}
-		public string NumberDecimalSeparator { get; set;}
-		public double Value { get; set; }
-		public bool IsPub { get; set; }
-	}
+    public class MQTTPubTopic : MQTTPubParam
+    {
+        public int NumCnl { get; set; }
+        public string PubBehavior { get; set; }
+        public string NumberDecimalSeparator { get; set; }
+        public double Value { get; set; }
+        public bool IsPub { get; set; }
+    }
 
-	public class MQTTPubCmd : MQTTPubParam
-	{
-		public int NumCmd { get; set;}
+    public class MQTTPubCmd : MQTTPubParam
+    {
+        public int NumCmd { get; set; }
 
 
-	}
+    }
 
-	public class RapSrvEx : ServerComm
-	{
-		public bool cn { get; set; }
+    public class RapSrvEx : ServerComm
+    {
+        public bool cn { get; set; }
 
-		public bool IsCurr { get; set; }
+        public bool IsCurr { get; set; }
 
-		private RapSrvEx rsrv;
+        private RapSrvEx rsrv;
 
-		public RapSrvEx (CommSettings cs)
-		{
-			rsrv = this;
-			rsrv.commSettings = cs;
+        public RapSrvEx(CommSettings cs)
+        {
+            rsrv = this;
+            rsrv.commSettings = cs;
 
-		}
+        }
 
-		public List<MQTTPubTopic>  GetValues (List<MQTTPubTopic> MqttPTs)
-		{
-			
-			if (cn) {
-				SrezTableLight stl = new SrezTableLight ();
-				IsCurr = rsrv.ReceiveSrezTable ("current.dat", stl);
-				SrezTableLight.Srez srez = stl.SrezList.Values [0];
-				bool found;
-				SrezTableLight.CnlData cnlData;
-				foreach (MQTTPubTopic MqttPT in MqttPTs) {
-					found = srez.GetCnlData (MqttPT.NumCnl, out cnlData);
-					if (found) {
-						if(MqttPT.PubBehavior=="OnChange")
-						{
-							if (MqttPT.Value != cnlData.Val)
-								MqttPT.IsPub = true;
-						}
-						if (MqttPT.PubBehavior == "OnAlways")
-							MqttPT.IsPub = true;
-						MqttPT.Value = cnlData.Val;
-					}
-				}
-			}
-			return MqttPTs;
-		}
+        public List<MQTTPubTopic> GetValues(List<MQTTPubTopic> MqttPTs)
+        {
 
-		public void Conn ()
-		{
-			cn = rsrv.Connect ();
-		}
+            if (cn)
+            {
+                SrezTableLight stl = new SrezTableLight();
+                IsCurr = rsrv.ReceiveSrezTable("current.dat", stl);
+                SrezTableLight.Srez srez = stl.SrezList.Values[0];
+                bool found;
+                SrezTableLight.CnlData cnlData;
+                foreach (MQTTPubTopic MqttPT in MqttPTs)
+                {
+                    found = srez.GetCnlData(MqttPT.NumCnl, out cnlData);
+                    if (found)
+                    {
+                        if (MqttPT.PubBehavior == "OnChange")
+                        {
+                            if (MqttPT.Value != cnlData.Val)
+                                MqttPT.IsPub = true;
+                        }
+                        if (MqttPT.PubBehavior == "OnAlways")
+                            MqttPT.IsPub = true;
+                        MqttPT.Value = cnlData.Val;
+                    }
+                }
+            }
+            return MqttPTs;
+        }
 
-		public void Disconn ()
-		{
-			rsrv.Disconnect ();
-		}
+        public void Conn()
+        {
+            cn = rsrv.Connect();
+        }
 
-	}
+        public void Disconn()
+        {
+            rsrv.Disconnect();
+        }
+
+    }
 
 }
 
