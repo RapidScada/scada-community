d97bbe7119a460c2bb904b12f1f4b29a86f050db
2mik <m_shir@mail.ru>
2025-09-18 17:26:16 +0300

    Initial commit of ScriptPlayground

---
+++ Scripts/ScriptPlayground/ScriptPlayground/PlaygroundCalcContext.cs
@@ -0,0 +1,43 @@
+﻿using Scada.Data.Models;
+using Scada.Server.Engine;
+
+namespace ScriptPlayground
+{
+    /// <summary>
+    /// Simulates access to channel data.
+    /// <para>Имитирует доступ к данным канала.</para>
+    /// </summary>
+    public class PlaygroundCalcContext : ICalcContext
+    {
+        private readonly Dictionary<int, CnlData> cnlDataDict = [];
+
+        public DateTime Timestamp => DateTime.UtcNow;
+
+        public bool IsCurrent => true;
+
+        public CnlData GetCnlData(int cnlNum)
+        {
+            return cnlDataDict.GetValueOrDefault(cnlNum, CnlData.Empty);
+        }
+
+        public CnlData GetPrevCnlData(int cnlNum)
+        {
+            return CnlData.Empty;
+        }
+
+        public DateTime GetCnlTime(int cnlNum)
+        {
+            return DateTime.UtcNow;
+        }
+
+        public DateTime GetPrevCnlTime(int cnlNum)
+        {
+            return DateTime.MinValue;
+        }
+
+        public void SetCnlData(int cnlNum, CnlData cnlData)
+        {
+            cnlDataDict[cnlNum] = cnlData;
+        }
+    }
+}
---
+++ Scripts/ScriptPlayground/ScriptPlayground/ScriptTester.cs
@@ -0,0 +1,17 @@
+﻿using Scada.Server.Engine;
+
+namespace ScriptPlayground
+{
+    /// <summary>
+    /// A base class for testing scripts.
+    /// <para>Базовый класс для тестирования скриптов.</para>
+    /// </summary>
+    public class ScriptTester : CalcEngine
+    {
+        public ScriptTester() 
+            : base()
+        {
+            calcContext = new PlaygroundCalcContext();
+        }
+    }
+}
