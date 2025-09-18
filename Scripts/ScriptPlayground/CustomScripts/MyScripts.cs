9b1b33520b701aff94ed3178a370a29c3b40a5ca
2mik <m_shir@mail.ru>
2025-09-18 17:46:07 +0300

    ScriptPlayground: example of scripts

---
+++ Scripts/ScriptPlayground/CustomScripts/MyScripts.cs
@@ -0,0 +1,36 @@
+﻿using ScriptPlayground;
+
+namespace CustomScripts
+{
+    /// <summary>
+    /// Represents an example of custom scripts.
+    /// </summary>
+    public class MyScripts : ScriptTester
+    {
+        /// <summary>
+        /// Linear transforms the input channel value.
+        /// </summary>
+        public double LinearTransform(double a, double b)
+        {
+            return a * Cnl + b;
+        }
+
+        /// <summary>
+        /// Calculates the sum of the values of the specified input channels.
+        /// </summary>
+        public double CnlValSum(params int[] cnlNums)
+        {
+            double sum = 0.0;
+
+            if (cnlNums != null)
+            {
+                foreach (int cnlNum in cnlNums)
+                {
+                    sum += Val(cnlNum);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Scripts/ScriptPlayground/CustomScriptsTest/MyScriptsTest.cs
@@ -0,0 +1,35 @@
+﻿using CustomScripts;
+using Scada.Data.Entities;
+using Scada.Data.Models;
+
+namespace CustomScriptsTest
+{
+    /// <summary>
+    /// Tests scripts.
+    /// </summary>
+    [TestClass]
+    public sealed class MyScriptsTest
+    {
+        [TestMethod]
+        public void LinearTransformTest()
+        {
+            MyScripts myScripts = new();
+            myScripts.BeginCalcCnlData(101, new Cnl(), new CnlData(100.0, 1));
+            double actualResult = myScripts.LinearTransform(5, 10);
+            Assert.AreEqual(510, actualResult);
+        }
+
+        [TestMethod]
+        public void CnlValSumTest()
+        {
+            MyScripts myScripts = new();
+            myScripts.SetData(101, 1.0, 1);
+            myScripts.SetData(102, 2.0, 1);
+            myScripts.SetData(103, 3.0, 1);
+            myScripts.SetData(104, 4.0, 1);
+            myScripts.SetData(105, 5.0, 1);
+            double actualResult = myScripts.CnlValSum(101, 102, 103, 104, 105);
+            Assert.AreEqual(15, actualResult);
+        }
+    }
+}
